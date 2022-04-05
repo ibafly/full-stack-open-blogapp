f64e04dd045c6a153e5cad91c7a385237aa4fd90
ibafly <copppe@gmail.com>
2022-04-05 18:17:40 +0800

    use webpack in frontend build process

--- index.js
+++ index.js
@@ -6,8 +6,44 @@ const http = require("http")
 //const logger = require("./utils/logger")
 const logger = require("@utils/logger")
 
+
+/**
+ * For frontend use hot loading when in development, else serve the static content
+ */
+if (!config.inProduction) {
+  /* eslint-disable */
+  const webpack = require('webpack')
+  const middleware = require('webpack-dev-middleware')
+  const hotMiddleWare = require('webpack-hot-middleware')
+  const webpackConf = require('@root/webpack.config')
+  /* eslint-enable */
+  const compiler = webpack(webpackConf('development', { mode: 'development' }))
+
+  const devMiddleware = middleware(compiler)
+  app.use(devMiddleware)
+  app.use(hotMiddleWare(compiler))
+  app.use('*', (req, res, next) => {
+    const filename = path.join(compiler.outputPath, 'index.html')
+    devMiddleware.waitUntilValid(() => {
+      compiler.outputFileSystem.readFile(filename, (err, result) => {
+        if (err) return next(err)
+        res.set('content-type', 'text/html')
+        res.send(result)
+        return res.end()
+      })
+    })
+  })
+} else {
+  const DIST_PATH = path.resolve(__dirname, './dist')
+  const INDEX_PATH = path.resolve(DIST_PATH, 'index.html')
+
+  app.use(express.static(DIST_PATH))
+  app.get('*', (req, res) => res.sendFile(INDEX_PATH))
+}
+
+
 const server = http.createServer(app)
 
 server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on PORT ${config.PORT}`)
 })
